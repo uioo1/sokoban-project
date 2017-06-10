50c6a309a0b0e0b67a082787a783b77eb0f70fbf
uioo1 <uioo1@naver.com>
2017-06-10 16:45:46 +0900

    많은 수정을 거친 최종본일지도

--- sokoban_alpha.c
+++ sokoban_alpha.c
@@ -3,60 +3,75 @@
 #include <termio.h>
 #include <time.h>
 
-int i = -1, j, k, stage, dl_x, dl_y, enter_judge, save_count, game_exit,save_stage;
-char get, game_act, get_enter;
-int map_current[5][30][30];
-int pl_x, pl_y,start_time[5],finish_time[5],diff_time[5],time_num=0,clear_num;
-int map_file[5][30][30] = {};
+int i = -1, j, k, stage = 0, clear_num, time_num = 0, undo_reset_num = 0, first_com_error = 0;
+int box_num, boxput_num;
+int pl_x, pl_y, game_exit = 0, save_stage, save_count = 0, dl_x, dl_y;
+int map_end[5][1], end_cnt;
+double start_time[5], finish_time[5], diff_time[5] = { 0 };
+char ch = 0, game_act, get;
+char map_current[5][30][30], map_load[30][30];
+char map_file[5][30][30] = { };
+char undo[5][30][30];
 char name[10];
-char ch = 0;
 
 void getname();
 void getmap();
 void showmap();
 void showcommand();
 void showname();
+void getkey();
+void time_set();
 void pl_move();
 void other_act();
-void time_set();
-void clear();
+void undo_save();
+void undo_reset();
+void clear(); 
 
-int getch(void) {
-	int ch;
+int getch(void){	//문자열을 입력받는 함수
+		int ch;
 
-	struct termios buf;
-	struct termios save;
+		struct termios buf;
+		struct termios save;
 
-	tcgetattr(0, &save);
-	buf = save;
+		tcgetattr(0, &save);
+		buf = save;
 
-	buf.c_lflag &= ~(ICANON | ECHO);
-	buf.c_cc[VMIN] = 1;
-	buf.c_cc[VTIME] = 0;
+		buf.c_lflag&=~(ICANON|ECHO);
+		buf.c_cc[VMIN] = 1;
+		buf.c_cc[VTIME] = 0;
+ 
+		tcsetattr(0, TCSAFLUSH, &buf);
 
-	tcsetattr(0, TCSAFLUSH, &buf);
-
-	ch = getchar();
-	tcsetattr(0, TCSAFLUSH, &save);
+		ch = getchar();
+		tcsetattr(0, TCSAFLUSH, &save);
 
 
-	return ch;
+		return ch;
 }
 
-int main(void) {
+
+int main(void){
 	getname();
 	getmap();
-
-	while (1) {
+	
+	while(game_exit != 1) {		
 		system("clear");
 		showname();
 		showmap();
 		showcommand();
+		time_set();
 		get = getch();
 		pl_move();
+		other_act();
+		clear();
 	}
+	printf("\nSee YOU %s\n\n\n", name);
+	showcommand();
+
 	return 0;
 }
+
+
 
 void getname(){	//이름을 입력받는 함수
 	system("clear");
@@ -113,7 +128,7 @@ void getmap(){	//map.txt에서 맵 불러와서 저장
 		}
 	}
 
-	for (i = 0; i<5; i++) {	//  원본 파일 그대로 복사
+	for (i = 0; i<5; i++) {
 		for (j = 0; j<30; j++) {
 			for (k = 0; k<30; k++) {
 				map_current[i][j][k] = map_file[i][j][k];
@@ -139,6 +154,7 @@ void getmap(){	//map.txt에서 맵 불러와서 저장
 }
 
 void showname(){
+
 	printf("    Hello ");
 	for (i = 0; i < 20; i++) {
 		printf("%c", name[i]);
@@ -176,7 +192,13 @@ void showmap(){
 }
 
 void showcommand(){
-	printf("(Command) %c", game_act);
+	if (first_com_error == 0) {
+		undo_reset();
+	}
+	else {
+		printf("(Command) %c", game_act);
+	}
+	first_com_error = 1;
 }
 
 void time_set() {
@@ -186,15 +208,10 @@ void time_set() {
 	}
 }
 
-void getkey() {
-	enter_judge = 0;
-	printf("%c", get); 
-	get_enter = getch();
-	if (get_enter == '\n')
-		enter_judge = 1;
-}
-
 void pl_move() {
+	if (get != 'u') {
+		undo_save();
+	}
 	switch (get) {
 	case 'l':
 		dl_x = 1, dl_y = 0;
@@ -363,134 +380,138 @@ void pl_move() {
 	default:
 		break;
 	}
+	game_act = ' ';
 }
+
 void other_act(){
 	switch (get) {
 	case 'r':	//현재 맵 처음부터 다시 시작
-		getkey();		
-		if (enter_judge == 1) {
-			for (j = 0; j < 30; j++) {
-				for (k = 0; k < 30; k++) {
-					map_current[stage][j][k] = map_file[stage][j][k];
-				}
+		for (j = 0; j < 30; j++) {
+			for (k = 0; k < 30; k++) {
+				map_current[stage][j][k] = map_file[stage][j][k];
 			}
 		}
 		game_act = 'r';
+		undo_reset_num = 0;
+		undo_reset();
 		break;
 	case 'n':	//첫번째 맵부터 다시 시작
-		getkey();		
-		if (enter_judge == 1) {
-			stage = 0;
-			for (i = 0; i < 5; i++) {
-				for (j = 0; j < 30; j++) {
-					for (k = 0; k < 30; k++) {
-						map_current[i][j][k] = map_file[i][j][k];
-					}
+		stage = 0;
+		for (i = 0; i < 5; i++) {
+			for (j = 0; j < 30; j++) {
+				for (k = 0; k < 30; k++) {
+					map_current[i][j][k] = map_file[i][j][k];
 				}
 			}
 		}
 		game_act = 'n';
+		undo_reset_num = 0;
+		undo_reset();
+		time_num = 0;
+
 		break;
 	case 'e':	//게임 종료, 종료하기전에 저장해야 함
-		getkey();		
-		if (enter_judge == 1) {
-			if (save_count == 1) {
-				game_exit = 1;
-			}
-			else {
-				printf("\n종료하기 전에 저장해야 합니다");
-			}
+		if (save_count == 1) {
+			game_exit = 1;
 		}
+		else {
+			printf("\n종료하기 전에 저장해야 합니다\n");
 		}
 		game_act = 'e';
 		break;
-	case 's': 	//현재 상태를 파일에 저장
-		getkey();		
-		if (enter_judge == 1) {
-			FILE *f = fopen("sokoban.txt", "w");
-			fprintf(f, "%d", stage);
-			int j, k;
-			for (j = 0; j < 30; j++) {
-				for (k = 0; k < 30; k++) {
-					fprintf(f, "%d", map_current[stage][j][k]);
-				}
+	case 's': {	//현재 상태를 파일에 저장	
+		FILE *f = fopen("sokoban.txt", "w");		
+		for (j = 0; j < 30; j++) {
+			for (k = 0; k < 30; k++) {
+				fprintf(f, "%d", map_current[stage][j][k]);
 			}
-			save_count = 1;
-			fclose(f);
 		}
-	}
-
-		}
+		fprintf(f, "%d", stage);
+		save_count = 1;
+		fclose(f);
 		game_act = 's';
 		break;
-	case 'd':	//명령 내용 보여주기
-		getkey();		
-		if (enter_judge == 1) {
-			getkey();
-			system("clear");
-			showname();
-			printf("\n");
-			printf("h(왼쪽), j(아래), k(위), l(오른쪽)\n");
-			printf("u(undo)\n");
-			printf("r(replay)\n");
-			printf("n(new)\n");
-			printf("e(exit)\n");
-			printf("s(save)\n");
-			printf("f(file load)\n");
-			printf("d(display help)\n");
-			printf("t(top)\n");
-			game_act = 'd';
-			showcommand();
-			printf("%c", get);
-			getch();
-		}
 	}
+	case 'd':	//명령 내용 보여주기
+		system("clear");
+		showname();
+		printf("\n");
+		printf("h(왼쪽), j(아래), k(위), l(오른쪽)\n");
+		printf("u(undo)\n");
+		printf("r(replay)\n");
+		printf("n(new)\n");
+		printf("e(exit)\n");
+		printf("s(save)\n");
+		printf("f(file load)\n");
+		printf("d(display help)\n");
+		printf("t(top)\n");
+		printf("/,*(치트키 오우예)\n\n");
+		game_act = 'd';
+		showcommand();
+		game_act = ' ';
+		getch();
+		
 		break;
-	case 'f': 	//저장된 내용을 불러오기
-		getkey();		
-		if (enter_judge == 1) {
-			FILE *f = fopen("sokoban.txt", "r");
-			fscanf(f, "%c", &save_stage);
-			for (j = 0; j < 30; j++) {
-				for (k = 0; k < 30; k++) {
-					fscanf(f, "%c", &ch);
-					map_current[save_stage][j][k] = ch - 48;
-				}
-			}
-			stage = save_stage - 48;
-			fclose(f);
-			game_act = 'f';
-				}
+	case 'f': {	//저장된 내용을 불러오기
+		FILE *f = fopen("sokoban.txt", "r");
+		for (j = 0; j < 30; j++) {
+			for (k = 0; k < 30; k++) {
+				fscanf(f, "%c", &ch);
+				map_load[j][k] = ch - 48;
 			}
 		}
+		fscanf(f, "%c", &save_stage);
+		stage = save_stage - 48;
+		for (j = 0; j < 30; j++) {
+			for (k = 0; k < 30; k++) {
+				map_current[stage][j][k] = map_load[j][k];
+			}
+		}
+		fclose(f);
+		game_act = 'f';
 		break;
+		}		
 	case 't':	//게임 순위 보여주기
-		getkey();		
-		if (enter_judge == 1) {
-			system("clear");
-			showname();
-			for (i = 0; i < 5; i++) {
-				printf("%.1f\n", diff_time[stage]);
-			}
-			showcommand();
-			printf("%c", get);
-			getch();
-
-		}
+		system("clear");
+		showname();
+		for (i = 0; i < 5; i++) {
+			printf("%.1f\n", diff_time[stage]);
 		}
 		game_act = 't';
+		showcommand();
+		game_act = ' ';
+		getch();
 		break;
 	case 'u':	//예전 상태로 돌아가기, 최대 5번까지
-		getkey();		
-		if (enter_judge == 1) {
-			
+		for (j = 0; j < 30; j++) {
+			for (k = 0; k < 30; k++) {
+				map_current[stage][j][k] = undo[0][j][k];
+			}
+		}
+		for (i = 0; i < 4; i++) {
+			for (j = 0; j < 30; j++) {
+				for (k = 0; k < 30; k++) {
+					undo[i][j][k] = undo[i + 1][j][k];
+				}
+			}
 		}
 		game_act = 'u';
+		break;
+	case '*':	//맵을 넘어가기 위한 치트키
+		if (stage != 4)
+			stage++;
+		game_act = '*';
+		break;
+	case '/':	//맵을 이전으로 가기 위한 치트키
+		if (stage != 0)
+			stage--;
+		game_act = '/';
 		break;
 	default:
 		break;
 	}
 }
+
 void clear(){	//게임을 클리어 했는지 판단하는 함수
 	clear_num = 0;
 	for (j = 0; j < 30; j++) {	//박스 놓는 곳이 있는지 판단하기
@@ -503,7 +524,9 @@ void clear(){	//게임을 클리어 했는지 판단하는 함수
 	if (clear_num == 0 && map_file[stage][pl_y + dl_y][pl_x + dl_x] != 4) {	//박스 놓는 곳이 없고 플레이어 위치가 박스 놓는 곳이 아니라면 클리어
 		finish_time[stage] = clock();
 		diff_time[stage] = (finish_time[stage] - start_time[stage]) * 2.5 / CLOCKS_PER_SEC;
+		printf("걸린시간: %.1f초", diff_time[stage]);
 		stage++;
+		undo_reset_num = 0;
 		time_num = 0;
 		getch();
 
@@ -511,4 +534,32 @@ void clear(){	//게임을 클리어 했는지 판단하는 함수
 			game_exit = 1;
 		}
 	}
+}
+
+void undo_save() {	//undo를 5개 저장하게 해주는 함수
+	for (i = 4; i > 0; i--) {
+		for (j = 0; j < 30; j++) {
+			for (k = 0; k < 30; k++) {
+				undo[i][j][k] = undo[i - 1][j][k];
+			}
+		}
+	}
+	for (j = 0; j < 30; j++) {
+		for (k = 0; k < 30; k++) {
+			undo[0][j][k] = map_current[stage][j][k];
+		}
+	}
+}
+
+void undo_reset() {	//초반에 undo 눌렀을때 안움직이게 해주고 r, n눌렀을때 undo 초기화
+	if (undo_reset_num == 0) {
+		for (i = 0; i<5; i++) {
+			for (j = 0; j<30; j++) {
+				for (k = 0; k<30; k++) {
+					undo[i][j][k] = map_file[stage][j][k];
+				}
+			}
+		}
+	}
+	undo_reset_num = 1;
 }
